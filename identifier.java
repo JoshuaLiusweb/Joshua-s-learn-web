9e67a682ff3d9c221e919cbda6d65e71eccc4228
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-17 16:20:38 +0800

    Add files via upload

---
+++ identifier.java
@@ -0,0 +1,10 @@
+public class Character
+{
+	public static void main(string[] args)
+	{
+		system.out.println(520)
+		system.out.println(5.20)
+		system.out.println('有‘）
+		system.out.println("我的真命天女在哪里“）
+	}
+}
