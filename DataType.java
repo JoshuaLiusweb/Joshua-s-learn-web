cb4493614ab69f11311acb0cffa2c3f5a2c08aab
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-18 20:53:52 +0800

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,25 @@
+public class DataType
+{
+	public static void main(String[] arges){
+		//下面会写一个普通的n字符
+		char c1='n';
+		System.out.println(c1);
+        char c2='\n';
+		//\n代表换行符，只代表一个字符，
+		System.out.print("A");
+		System.out.print(c2);
+		System.out.print("B");
+		char x='t';
+		char y='\t';
+		System.out.print("A");
+		System.out.print(y);
+		System.out.print("B");
+		char z='\\';
+		System.out.println(z);
+        char t='\'';
+		System.out.println(t);
+		System.out.println("HelloWorld");
+		System.out.println("“HelloWorld”");
+		System.out.println("\"HelloWorld\"");
+	}
+}
