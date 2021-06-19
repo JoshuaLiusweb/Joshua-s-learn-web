598ed6b5f043a0e276f01df5c5636b566ea1f9a6
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-20 00:10:54 +0800

    Add files via upload

--- Grade.java
+++ Grade.java
@@ -1,16 +1,19 @@
 public class Grade{
 	public static void main(String[] arges){
-		int Grade=90;
-		if (Grade>=90)
-			{System.out.println('A');
-		}else if (Grade>=80)
-			{System.out.println('B');
-		}else if (Grade>=70)
-			{System.out.println('C');
-		}else if (Grade >= 60)
-			{System.out.println('D');
-		}else
-			{System.out.println('F');}
-	}
+		int score=91;
+		String Grade="该考生成绩为F";
+		if (score<0||score>100){
+			Grade="该考生的成绩为：不合法";
+		}else if (score>=90){//此时score一定在【0-100】内
+			Grade="该考生的成绩为：A";
+		}else if (score>=80){
+			Grade="该考生的成绩为：B";
+		}else if (score >= 70){
+			Grade="该考生的成绩为：C";
+		}else if (score >= 60){
+			Grade="该考生的成绩为：D";
+	    }
+		System.out.println(Grade);
+		}
 }
 
