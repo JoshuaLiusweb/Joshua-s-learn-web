f4919cd1c2f2e3c4f59f45b680234eaa8995766d
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-19 23:31:52 +0800

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,16 @@
+public class Grade{
+	public static void main(String[] arges){
+		int Grade=90;
+		if (Grade>=90)
+			{System.out.println('A');
+		}else if (Grade>=80)
+			{System.out.println('B');
+		}else if (Grade>=70)
+			{System.out.println('C');
+		}else if (Grade >= 60)
+			{System.out.println('D');
+		}else
+			{System.out.println('F');}
+	}
+}
+
