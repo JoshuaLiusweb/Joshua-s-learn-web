d8459a51a7ff8cae6738c1778eefd29825aa6e7e
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-20 22:06:22 +0800

    Add files via upload

--- Count.java
+++ Count.java
@@ -1,46 +1,46 @@
-public class Count{
-	public static void main(String[] args[]){
+//public class Count
+//{
+	//public static void main (String[] args)
+	//{
+		//for (int i=1; i<=100 ;i+=2 )
+			//{System.out.print("奇数--》"+i);
+		//}
 		
-		java.util.Scanner s= new java.util.Scanner(System.in);
-		System.out.println("欢迎使用本程序");
-		
-		System.out.print("请输入第一个数字:");
-		int.num1 = s.nextint();
-		
-		System.out.print("请输入计算符:");
-		String operator = s.next();
-		
-		System.out.print("请输入第二个数字:");
-		int.num2 = s.nextint();
-		
-		int result = 0;
+	//}
+//方法一
 
-		switch (operator)
+//public class Count
+//{
+	//public static void main (String[] args)
+	//{
+		//for (int i=1; i<=100 ;i++ )
+			//{
+			//System.out.print("i--》"+i);
+			//if (i%2!=0)
+				//{System.out.print(i);
+			//}
+		//}
+		
+	//}
+//}
+
+//计算1-100的奇数累加和
+public class Count
+{
+	public static void main (String[] args)
+	{
+		int sum =0;
+
+		for (int i=1; i<=100 ;i+=2 )
 			{
-				case "+" :
-					result = num1 + num2;
-					break;
+				//先定义一个变量充当盒子
+				//int sum =0;代码在循环体中就每次都先清零
+				sum +=i;
 
-				case "-" :
-					result = num1 - num2;
-					break;
-
-				case "*" :
-					result = num1 * num2;
-				    break;
-
-				case "/" :
-					result = num1 / num2;	
-					break;
-
-				case "%" :
-					result = num1 % num2;
-					break;
-				default:
-					System.out.println("对不起，该系统不支持该种运算");
-		
+				//输出语句放到这表示每执行一次就输出一次
+				// System.out.print("sum="+sum);
 			}
-		
-		System.out.print("运算结果为"+num1+ operator +num2+ "=" +result);
+		System.out.print("sum="+sum);
 	}
 }
+
