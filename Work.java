4f1eaa7b0942918957f84c90840b521d1e3503b7
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-21 21:00:47 +0800

    Add files via upload

---
+++ Work.java
@@ -0,0 +1,27 @@
+public class Work
+{
+	java.util.Scanner s= new java.util.Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		System.out.println("本系统仅供判断二十四位人格使用");
+		System.out.print("请输入名字:");
+		System.out.println("说明1：1表示/，0表示/");
+		
+		String name=next();
+
+		switch(name)
+		{
+			case name="/"：
+				for(int i=1;i<=100;i++)
+				System.out.println("天才");
+	
+			case name=="/"：
+				for (int i=1;i<=100 ; i++)
+				{System.out.println("小傻瓜");
+		
+				}
+		}
+
+	}
+}
