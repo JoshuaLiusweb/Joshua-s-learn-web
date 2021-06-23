fe505c68767109bd0fa6f6fe4f448d0d31be300e
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-06-23 17:37:28 +0800

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,54 @@
+public class Homework
+{
+	/*public static void main(String[] args)
+	{
+		//先编写程序验证7是否为素数
+		/*7%1=0
+		7%2！=0
+		7%3! =0
+		7%4! =0
+		7%5! =0
+		7%6！=0
+		7%7！=0
+		
+		int i=0;
+		boolean issushu =true;//现假设他是素数
+		for (int j=2;j<i ;i++ )
+			{
+				if (i%j==0)
+				{
+					issushu=false;
+					break;
+				}
+			}
+		System.out.print(issushu? i+"是素数":i+"不是素数")
+	}*/
+
+	public static void main(String[] args)
+	{
+		//先编写程序验证7是否为素数
+		/*7%1=0
+		7%2！=0
+		7%3! =0
+		7%4! =0
+		7%5! =0
+		7%6！=0
+		7%7！=0
+		*/
+		int i=1;
+		for (;i<=100 ;i++ )
+		{
+		
+		boolean issushu =true;//现假设他是素数
+		for (int j=2;j<i ;j++ )
+			{
+				if (i%j==0)
+				{
+					issushu=false;
+					break;
+				}
+			}
+		System.out.print(issushu? i+"是素数":i+"不是素数");
+		}
+	}
+}
