c68011cb3e3fcb696daea8604129502a5ca528e0
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-07-01 19:48:48 +0800

    Add files via upload

---
+++ TestTime.java
@@ -0,0 +1,22 @@
+public class TestTime
+{
+	public static void main(String[] arges)
+	{
+		int a=10;
+		int b=20;
+		int retValue=sumInt(a,b);
+		System.out.println("retValue = " +retValue);
+	}
+	public static int sumInt ( int i ,int j)
+	{
+		int result = i+j;
+		int num =3;
+		int retValue = divide(result,num);
+		return retValue;
+	}
+	public static int divide (int x ,int y)
+	{
+		int z = x/y;
+		return z;
+	}
+}
