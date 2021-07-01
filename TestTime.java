97a20be71e61f97a126817fc673ce45aad1c9a85
JoshuaLiusweb <85872496+JoshuaLiusweb@users.noreply.github.com>
2021-07-01 20:21:44 +0800

    Add files via upload

--- TestTime.java
+++ TestTime.java
@@ -1,22 +1,66 @@
-public class TestTime
+/**public class TestTime
 {
+	//以下不使用方法重载
 	public static void main(String[] arges)
 	{
-		int a=10;
-		int b=20;
-		int retValue=sumInt(a,b);
-		System.out.println("retValue = " +retValue);
+		int result1 =sumInt(1,2);
+		System.out.println(result1);
+
+		double result2=sumDouble(1.0,2.0);
+		System.out.println(result2);
+
+		long result3 = sumLong(1L,2L);
+		System.out.println(result3);
 	}
+	
 	public static int sumInt ( int i ,int j)
 	{
-		int result = i+j;
-		int num =3;
-		int retValue = divide(result,num);
-		return retValue;
+		return i+j;
 	}
-	public static int divide (int x ,int y)
+	
+	public static double sumDouble ( double a ,double b)
 	{
-		int z = x/y;
-		return z;
+		return a+b;
+	}
+	
+	public static long sumLong ( long x ,long y)
+	{
+		return x+y;
 	}
 }
+*/
+
+
+public class TestTime
+{
+	//以下使用方法重载
+	public static void main(String[] arges)
+	{
+		int result1 =sum(1,2);
+		System.out.println(result1);
+
+		double result2=sum(1.0,2.0);
+		System.out.println(result2);
+
+		long result3 = sum(1L,2L);
+		System.out.println(result3);
+	}
+	
+	public static int sum ( int x ,int y)
+		{
+		return x+y;
+	}
+	public static double sum ( double x ,double y)
+		{
+		return x+y;
+	}
+	public static long sum ( long x ,long y)
+	{
+		return x+y;
+	}
+	
+}
+
+
+
+
